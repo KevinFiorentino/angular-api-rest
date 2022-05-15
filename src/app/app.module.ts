d991c688d8e87f34ea1472bd826df7aabed72150
Kevin <kevinfiorentino.1294@gmail.com>
2022-05-15 15:11:03 -0300

    Interceptores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,10 @@ import { CatalogoComponent } from './components/catalogo/catalogo.component';
 import { ProductoComponent } from './components/producto/producto.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { ErrorsInterceptor } from './interceptors/errors.interceptor';
+import { TokenInterceptorService } from './interceptors/token-interceptor.interceptor';
 
 @NgModule({
   declarations: [
@@ -17,6 +21,7 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
     ProductoComponent,
     NavBarComponent,
     SideBarComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,7 +29,18 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorsInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
